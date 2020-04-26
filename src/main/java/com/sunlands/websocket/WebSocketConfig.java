4b4d0b1a7e21b3713bf8abc912c6f8a717f2044e
chenweijie <chenweijie@sunlands.com>
2020-04-26 18:44:15 +0800

    first commit

---
+++ src/main/java/com/sunlands/websocket/WebSocketConfig.java
@@ -0,0 +1,18 @@
+package com.sunlands.websocket;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.server.standard.ServerEndpointExporter;
+
+/**
+ * @author Chen WeiJie
+ * @date 2020-04-26 18:17:50
+ **/
+@Configuration
+public class WebSocketConfig {
+
+    @Bean
+    public ServerEndpointExporter serverEndpointExporter() {
+        return new ServerEndpointExporter();
+    }
+}
